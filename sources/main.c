6dc83e0315f2bff9f3b6738cbaa1994d882670f1
nymarya <mayradazevedo@gmail.com>
2016-11-23 19:45:42 +0000

    fator deslocamento como parametro na linha de comando

--- sources/main.c
+++ sources/main.c
@@ -11,18 +11,36 @@ int main(int n, char **str)
 {
     
     char nome_do_arquivo[30];
+    int desloc = 128;
     strcpy(nome_do_arquivo, "terreno.ppm");
 
 	if(n>1){
 	    if(strcmp(str[1], "-o") == 0){
-		strcpy(nome_do_arquivo, str[2]);
+		  strcpy(nome_do_arquivo, str[2]);
+		  
+		  if(n>3){
+    		  if(strcmp(str[3], "-d") == 0){
+    		      desloc = atoi(str[4]);
+    		  }
+		  }
+
+	    }
+	    else if(strcmp(str[1], "-d") == 0){
+	        desloc = atoi(str[2]);
+	        
+	        if(n>3){
+    	      if(strcmp(str[3], "-o") == 0){
+    		      strcpy(nome_do_arquivo, str[4]);
+    		  }
+		    }
+
 	    }
 	}
         
 
 	int tam = 513; //2^9+1
 
-	int desloc = 128;
+	
 	int vetor[tam]; 
 	PIXEL matriz[tam][tam];
 
