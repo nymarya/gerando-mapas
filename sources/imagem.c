a85b9d9aa4a987249b10fd590a0f766cb070c5b9
Mayra Azevedo <mayradazevedo@gmail.com>
2016-12-05 22:11:05 -0200

    Corrigindo identação

--- sources/imagem.c
+++ sources/imagem.c
@@ -12,10 +12,10 @@
  * @param b      Valor do azul
  */
 void configuraPixel(PIXEL *pixel, int r, int g, int b){
-
-	pixel->r = r;
-	pixel->g = g;
-	pixel->b = b;
+	
+     pixel->r = r;
+     pixel->g = g;
+     pixel->b = b;
 }
 
 /**
@@ -26,18 +26,18 @@ void configuraPixel(PIXEL *pixel, int r, int g, int b){
  */
 int gerarMatriz(int tam, PIXEL matriz[tam][tam], int vetor[tam]){
 	
-	int i, j;
-	for (i = 0; i < tam; i++) {
-		for (j = 0; j < tam; j++) {
+    int i, j;
+    for (i = 0; i < tam; i++) {
+	for (j = 0; j < tam; j++) {
 
-			if (i > vetor[j]) {
-				configuraPixel(&matriz[i][j], 1, 2, 18);
-			} 
+	    if (i > vetor[j]) {
+		configuraPixel(&matriz[i][j], 1, 2, 18);
+	    } 
 				
-		}
 	}
+    }
 	
-	return 0;
+    return 0;
 
 }
 
@@ -50,28 +50,28 @@ int gerarMatriz(int tam, PIXEL matriz[tam][tam], int vetor[tam]){
  */
 int escreverImagem(int tam, PIXEL matriz[tam][tam], char nome_do_arquivo[30]){
 	
-	char *endereco = "../galeria/"; //endereço padrão
-	char *caminho = malloc(strlen(endereco)+strlen(nome_do_arquivo)+1);
-	strcpy(caminho, endereco); //copia endereço para variável
-	strcat(caminho, nome_do_arquivo); //concatena endereço e nome do arquivo
-	FILE *arquivo;
-	arquivo = fopen(caminho, "w" );
+    char *endereco = "../galeria/"; //endereço padrão
+    char *caminho = malloc(strlen(endereco)+strlen(nome_do_arquivo)+1);
+    strcpy(caminho, endereco); //copia endereço para variável
+    strcat(caminho, nome_do_arquivo); //concatena endereço e nome do arquivo
+    FILE *arquivo;
+    arquivo = fopen(caminho, "w" );
 	
 	
-	fprintf(arquivo, "P3\n");
-	fprintf(arquivo, "%i %i\n", tam, tam);
-	fprintf(arquivo, "%i\n", 255);
+    fprintf(arquivo, "P3\n");
+    fprintf(arquivo, "%i %i\n", tam, tam);
+    fprintf(arquivo, "%i\n", 255);
 
-	int i, j;
-	for(i=0; i<tam; i++){
-		for(j=0; j<tam; j++){
-			fprintf(arquivo, "%i %i %i", matriz[i][j].r, matriz[i][j].g, matriz[i][j].b);
-			fprintf(arquivo, "\n");
-		}
-		fprintf(arquivo, "\n");
+    int i, j;
+    for(i=0; i<tam; i++){
+	for(j=0; j<tam; j++){
+	    fprintf(arquivo, "%i %i %i", matriz[i][j].r, matriz[i][j].g, matriz[i][j].b);
+	    fprintf(arquivo, "\n");
 	}
+	fprintf(arquivo, "\n");
+    }
 	
-	free(caminho);
-	fclose(arquivo);
-	return 0;
+    free(caminho);
+    fclose(arquivo);
+    return 0;
 }
